cf251fe299d6296a13882727b067151503f09f0e
trunya <144317215+trunya@users.noreply.github.com>
2024-03-06 13:36:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Webhook succesfully built!\n";
   return 0;
 }
